3b72b8b32d43543aea7640fe9964666730c6773a
hariomtiwari <hariomtiwari.22.88@gmail.com>
2025-02-17 23:08:10 +0530

    GET-POST-DELETE

--- src/app.js
+++ src/app.js
@@ -26,6 +26,19 @@ app.post('/signup', async (req, res) => {
  
 })
 
+app.get('/user', async (req, res) => {
+  const emailId = req.body.emailId;
+  const users = await User.find({emailId:emailId});
+  try{
+    if(users.length==0){
+      res.send('No Such User Exists with the given Record');
+    }
+     else res.send(users);
+  }catch(err){
+    res.status(400).send('Error Occured During getting the User' + err.message);
+  }
+})
+
 connectDB().then(() => {
 console.log('Connnection Established SuccessFully');
 app.listen(7777, () => console.log('Server Listening at Port 7777'));
